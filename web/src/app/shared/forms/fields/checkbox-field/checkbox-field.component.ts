cd905a7185f3ed6210c64cfda28e4eb19f610fd4
Alexander Bachmann <kontakt@alexanderbachmann.de>
2017-07-14 16:43:06 +0200

    Add Checkbox Field Component

--- web/src/app/shared/forms/fields/checkbox-field/checkbox-field.component.ts
+++ web/src/app/shared/forms/fields/checkbox-field/checkbox-field.component.ts
@@ -6,72 +6,94 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, Input, forwardRef } from '@angular/core';
+import { AbstractField } from '../abstract-field';
+import { FormControl, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
 	selector: 'tekkl-checkbox-field',
 	templateUrl: './checkbox-field.component.html',
-	styleUrls: ['./checkbox-field.component.sass']
+	styleUrls: ['./checkbox-field.component.sass'],
+	providers: [{ 
+		provide: NG_VALUE_ACCESSOR,
+		useExisting: forwardRef(() => CheckboxFieldComponent),
+		multi: true
+	}]
 })
-export class CheckboxFieldComponent implements OnInit {
-	@Input() control: FormControl
-	@Input() value: any
+export class CheckboxFieldComponent extends AbstractField implements ControlValueAccessor {
+	@Input() value: string | boolean = true
 	@Input() label: string
-	@Input() name: string;
-	@Input() multiple: any
-	@Output() change: EventEmitter<any> = new EventEmitter()
-	onChange($event){
-		this.change.emit($event.target.value);
-	}
+	@Input() multiple: string | boolean | undefined
+
+	propagateChange = (_: any) => {};
+	propagateTouch = () => {};
+	
 	ngOnInit() {
 		this.multiple = (typeof(this.multiple) === 'undefined') ? false : true;
-		// Cast value to a string;
-		this.value = '' + this.value;
-		
 	}
 	toggle($event){
+		// Don't toggle if user clicked on an anchor link within the label
+		if($event.target.nodeName == 'A') return;
 		if(this.isChecked()){
 			this.uncheck();
 		}else{
 			this.check();
 		}
+		this.propagateChange(this.getFormControlValue());
+		this.propagateTouch();
 	}
-	check(){
+	writeValue(value: string){
+		return;
+	}
+	registerOnChange(fn: (_:any) => {}){
+		this.propagateChange = fn;
+	}
+	registerOnTouched(fn: () => {}){
+		this.propagateTouch = fn;
+	}
+	private getFormControlValue(){
+		var value = this.getFormControl().value;
+		if(this.multiple){
+			if(typeof(value) == 'string'){
+				throw new Error('The value of a checkbox with the attribute \'multiple\' must be an array');
+			}
+			if(!value || value.length === undefined){
+				value = [];
+			}
+		}
+		return value;
+	}
+	private check(){
 		if(this.isChecked()) return;
 		if(this.multiple){
-			var values = this.control.value;
-			if(!values || !values.length){
-				values = [];
+			var values = this.getFormControlValue();
+			if(values.indexOf(this.value) < 0){
+				values.push(this.value);
 			}
-			values.push(this.value);
-			this.control.setValue(values);
+			this.getFormControl().setValue(values);
 		}else{
-			this.control.setValue(this.value);
+			this.getFormControl().setValue(this.value);
 		}
 	}
-	uncheck(){
+	private uncheck(){
+		if(!this.isChecked()) return;
 		if(this.multiple){
-			var values = this.control.value;
-			if(!values || !values.length){
-				values = [];
-			}
+			var values = this.getFormControlValue();
 			var index = values.indexOf(this.value);
 			if(index >= 0){
 				values.splice(index, 1);
 			}
-			this.control.setValue(values);
+			this.getFormControl().setValue(values);
 		}else{
-			this.control.setValue(null);
+			this.getFormControl().setValue(null);
 		}
 	}
 	isChecked(){
-		var values = this.control.value;
-		if(!this.multiple){
-			values = [values];
-		}else if(!values || !values.length){
-			values = [];
+		var values = this.getFormControlValue();
+		if(this.multiple){
+			return (values.indexOf(this.value) >= 0);
+		}else {
+			return values === this.value;
 		}
-		return (values.indexOf(this.value) >= 0);
 	}
 }
