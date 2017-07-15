323385ce2a38624e1e62f0987b50f41901099f76
Alexander Bachmann <kontakt@alexanderbachmann.de>
2017-07-15 10:39:27 +0200

    Add Doctrine Migration Bundle

--- engine/AppKernel.php
+++ engine/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
