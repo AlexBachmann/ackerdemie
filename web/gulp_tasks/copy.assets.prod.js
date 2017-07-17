6f1160762afb23fbd64dfc0f7dbd772e191f368c
Alexander Bachmann <kontakt@alexanderbachmann.de>
2017-07-17 15:44:04 +0200

    Bugfix when building for production

--- web/gulp_tasks/copy.assets.prod.js
+++ web/gulp_tasks/copy.assets.prod.js
@@ -1,9 +1,14 @@
 "use strict";
 
-var config = require('./config');
+var config = require('./config'),
+	rename = require('gulp-rename');
+	
 module.exports = function (gulp, plugins, options) {
     return function () {
-        return 	gulp.src([config.paths.src + '/.htaccess', config.paths.src + '/app.php'])
+        gulp.src([config.paths.src + '/app.php'])
         		.pipe(gulp.dest(config.paths.dist));
+        return gulp.src([config.paths.src + '/.htaccess.prod'])
+        		.pipe(rename('.htaccess'))
+        		pipe(gulp.dest(config.paths.dist));
     };
 };
