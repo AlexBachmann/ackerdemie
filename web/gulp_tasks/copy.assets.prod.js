b1443093ab7142b97cf583314bb03415c4e62b88
Alexander Bachmann <kontakt@alexanderbachmann.de>
2017-07-18 07:18:25 +0200

    change how assets are being copied during production build

--- web/gulp_tasks/copy.assets.prod.js
+++ web/gulp_tasks/copy.assets.prod.js
@@ -1,14 +1,15 @@
 "use strict";
 
 var config = require('./config'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	fs = require('fs'),
+	path = require('path'),
+	join = path.join;
 	
 module.exports = function (gulp, plugins, options) {
     return function () {
         gulp.src([config.paths.src + '/app.php'])
         		.pipe(gulp.dest(config.paths.dist));
-        return gulp.src([config.paths.src + '/.htaccess.prod'])
-        		.pipe(rename('.htaccess'))
-        		pipe(gulp.dest(config.paths.dist));
+        fs.createReadStream(join(process.cwd(), config.paths.src, '.htaccess.prod')).pipe(fs.createWriteStream(join(process.cwd(), config.paths.dist, '.htaccess')));
     };
 };
